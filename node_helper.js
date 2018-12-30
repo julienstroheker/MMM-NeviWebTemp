bcd9cfc017d33ef99e3be27f2ab924967a2e7507
Julien Stroheker <juliens@microsoft.com>
2018-12-30 17:57:31 -0500

    Sort Array - Fix Layout - Remove Icons

--- node_helper.js
+++ node_helper.js
@@ -5,79 +5,103 @@
  * MIT Licensed.
  */
 
-var NodeHelper = require('node_helper');
-var request = require('request');
-
+var NodeHelper = require("node_helper");
+var request = require("request");
 
 module.exports = NodeHelper.create({
-  start: function () {
-    console.log('MMM-NeviWebTemp helper started ...');
+  start: function() {
+    console.log("MMM-NeviWebTemp helper started ...");
   },
 
-//{'dataServer' 'email' 'password' 'gatewayname' 'room'});
+  //{'dataServer' 'email' 'password' 'gatewayname' 'room'});
   getTemp: function(api_url) {
     var self = this;
     var uri = api_url.dataServer;
-    var path = uri+'api/login';
-    var result = {"gatewayID": 1, "gatewayName": "", "thermostats": []};
+    var path = uri + "api/login";
+    var result = { gatewayID: 1, gatewayName: "", thermostats: [] };
 
-    dataPayload = {'email': api_url.email, 'password': api_url.password, 'stayConnected': '0'};
+    dataPayload = {
+      email: api_url.email,
+      password: api_url.password,
+      stayConnected: "0"
+    };
 
-    request({url: path, method: 'POST', form: dataPayload}, function(error, response, body) {
+    request({ url: path, method: "POST", form: dataPayload }, function(
+      error,
+      response,
+      body
+    ) {
       if (!error && response.statusCode == 200) {
-        if((JSON.parse(body).session)!=null) {
+        if (JSON.parse(body).session != null) {
           var sessionId = JSON.parse(body).session;
-          var path = uri+'api/gateway';
-          var dataHeaders = {'Session-Id': sessionId};
+          var path = uri + "api/gateway";
+          var dataHeaders = { "Session-Id": sessionId };
 
-          request({url: path, method: 'GET', headers: dataHeaders}, function(error, response, body) {
+          request({ url: path, method: "GET", headers: dataHeaders }, function(
+            error,
+            response,
+            body
+          ) {
             if (!error && response.statusCode == 200) {
               var gatewayId = JSON.parse(body)[0].id;
               result.gatewayID = gatewayId;
               result.gatewayName = JSON.parse(body)[0].name;
-              var path = uri+'api/device';
-              var params = {'gatewayId': gatewayId};
+              var path = uri + "api/device";
+              var params = { gatewayId: gatewayId };
 
-              request({url: path, method: 'GET', headers: dataHeaders, qs: params}, function(error, response, body) {
-                if (!error && response.statusCode == 200) {
+              request(
+                { url: path, method: "GET", headers: dataHeaders, qs: params },
+                function(error, response, body) {
+                  if (!error && response.statusCode == 200) {
+                    JSON.parse(body).forEach(function(device) {
+                      var thermoinfos = {
+                        name: "",
+                        id: null,
+                        active: null,
+                        setpoint: null,
+                        temperature: null
+                      };
+                      var path =
+                        uri + "api/device/" + device.id + "/data?force=1";
+                      thermoinfos.name = device.name;
+                      thermoinfos.id = device.id;
 
-                  JSON.parse(body).forEach(function(device) {
-                    var thermoinfos = { "name": "", id: null, "active" : null, "setpoint" : null, "temperature" : null };
-                    var path = uri+'api/device/' + device.id + '/data?force=1';
-                    thermoinfos.name = device.name;
-                    thermoinfos.id = device.id;
-                    
-                    request({url: path, method: 'GET', headers: dataHeaders}, function(error, response, body) {
-                      if (!error && response.statusCode == 200) {
-
-                        thermoinfos.temperature = JSON.parse(body).temperature;
-                        thermoinfos.setpoint = JSON.parse(body).setpoint;
-                        if(JSON.parse(body).heatLevel >= 1){thermoinfos.active = 1;}
-                        else{thermoinfos.active=0;}
-                        result.thermostats.push(thermoinfos);
-                      }
-                      else
-                      {
-                        console.log("Error to fetch infos from devices");
-                      }
-                    });
-                  }, this);
-                  setTimeout(function() {self.sendSocketNotification('NEVI_MYTEMP', result); }, 2000);                  
+                      request(
+                        { url: path, method: "GET", headers: dataHeaders },
+                        function(error, response, body) {
+                          if (!error && response.statusCode == 200) {
+                            thermoinfos.temperature = JSON.parse(
+                              body
+                            ).temperature;
+                            thermoinfos.setpoint = JSON.parse(body).setpoint;
+                            if (JSON.parse(body).heatLevel >= 1) {
+                              thermoinfos.active = 1;
+                            } else {
+                              thermoinfos.active = 0;
+                            }
+                            result.thermostats.push(thermoinfos);
+                          } else {
+                            console.log("Error to fetch infos from devices");
+                          }
+                        }
+                      );
+                    }, this);
+                    setTimeout(function() {
+                      result.thermostats.sort(function(a, b) {
+                        return parseInt(a.id) - parseInt(b.id);
+                      });
+                      self.sendSocketNotification("NEVI_MYTEMP", result);
+                    }, 2000);
+                  } else {
+                    console.log("Error with the devices");
+                  }
                 }
-                else
-                {
-                  console.log("Error with the devices");
-                }
-              });
-            }
-            else
-            {
+              );
+            } else {
               console.log("Error with the gateway");
             }
-          }); 
-        }
-        else
-        {
+          });
+        } else {
           console.log("Error with session ID");
         }
       }
@@ -86,8 +110,8 @@ module.exports = NodeHelper.create({
 
   //Subclass socketNotificationReceived received.
   socketNotificationReceived: function(notification, payload) {
-    if (notification === 'NEVI_TEMP') {
+    if (notification === "NEVI_TEMP") {
       this.getTemp(payload);
-    } 
+    }
   }
 });
